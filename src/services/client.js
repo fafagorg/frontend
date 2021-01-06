fa88273223bf0106bccce9cd56b73cebc9ee8f09
mc <maricarmenarenas98@hotmail.es>
2021-01-06 17:34:45 +0100

    feat(): new token

--- src/services/client.js
+++ src/services/client.js
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken";
 export const getJWT = () => {
   // let token = new Cookies().get('access_token') || null
   let token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjF9.-XWGATiWn40-XFP-yRccxl5B6rLGW-fv2auhlP5PXrE"; // remove
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjVmZjRhNzlkZmM2MGM4NDdkNmViY2NjMiIsInVzZXJuYW1lIjoic3RyaW5nIiwibmFtZSI6InN0cmluZyIsInN1cm5hbWUiOiJzdHJpbmciLCJlbWFpbCI6InN0cmluZyIsInBob25lIjoiMTIzNDU2Nzg5IiwiX192IjowfSwiaWF0IjoxNjA5OTM4MjQwLCJleHAiOjE2MTAwMjQ2NDB9.StVDDYp29rpGaTglxtgsw__wqudlxEiAUpVL-1ue_OI"; // remove
   new Cookies().set('access_token', token) // remove
   return {
     token: token,

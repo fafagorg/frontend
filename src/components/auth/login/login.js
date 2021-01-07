bc38d11ab8d6086b922e66919f008ad5ccb65594
Roberto Hermoso <rhermoso98@gmail.com>
2021-01-07 19:55:50 +0100

    feat: login component

--- src/components/auth/login/login.js
+++ src/components/auth/login/login.js
@@ -1,18 +1,63 @@
 import React from "react";
+import * as AuthService from "../../../services/auth";
+import Cookies from "universal-cookie";
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {username: '',
+                  password: '',
+                  errors: {
+                    username: '', 
+                    password: '',
+                    login: ''
+                  }};
+
+
+    var users = AuthService.getUsers()
+    users.then(x=>{
+      console.log(x)
+    })
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    var name = event.target.name
+    this.setState({[name] : event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    AuthService.login({
+      username: this.state.username,
+      password: this.state.password
+    }).then(res=>{
+      var token = res.token
+      new Cookies().set('access_token', token)
+    }).catch(err=>{
+      this.setState({errors: {login: 'Username or password are wrong'}})
+    })
+    //event.preventDefault();
+  }
+
   render() {
     return (
-      <form class='form-group'> 
-        <fieldset>
-          <div class="form-group row">
-          <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-          <div class="col-sm-10">
-          </div>
-              <input type="text" class="form-control-plaintext" id="staticEmail" value="email@example.com"></input>
-          </div>
-        </fieldset>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Username:
+          <input type="text" value={this.state.username} name='username' onChange={this.handleChange} />
+        </label>
+        <label>
+          Password
+          <input type="password" value={this.state.password} name='password' onChange={this.handleChange}/>
+        </label>
+        <input type="submit" value="Submit" />
+        {true == true && (
+          <div class="error">{this.state.errors.login} </div>)}
       </form>
+      
     );
   }
 }

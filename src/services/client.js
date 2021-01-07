e2610b2b918e80ebf21aba8a5e717f591d149a49
Jesús Monda <jesusmonda9@gmail.com>
2021-01-07 21:25:53 +0100

    messenger finished

--- src/services/client.js
+++ src/services/client.js
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken";
 export const getJWT = () => {
   // let token = new Cookies().get('access_token') || null
   let token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjVmZjRhNzlkZmM2MGM4NDdkNmViY2NjMiIsInVzZXJuYW1lIjoic3RyaW5nIiwibmFtZSI6InN0cmluZyIsInN1cm5hbWUiOiJzdHJpbmciLCJlbWFpbCI6InN0cmluZyIsInBob25lIjoiMTIzNDU2Nzg5IiwiX192IjowfSwiaWF0IjoxNjA5OTM4MjQwLCJleHAiOjE2MTAwMjQ2NDB9.StVDDYp29rpGaTglxtgsw__wqudlxEiAUpVL-1ue_OI"; // remove
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjVmZjRhNzlkZmM2MGM4NDdkNmViY2NjMiIsInVzZXJuYW1lIjoic3RyaW5nIiwibmFtZSI6InN0cmluZyIsInN1cm5hbWUiOiJzdHJpbmciLCJlbWFpbCI6InN0cmluZyIsInBob25lIjoiMTIzNDU2Nzg5IiwiX192IjowfSwiaWF0IjoxNjEwMDQxOTc0LCJleHAiOjE2MTAxMjgzNzR9.Wj5bgY_xY1e92rpYt4arIHAVoyLjV_WrxZ-l9EoBPFY"; // remove
   new Cookies().set('access_token', token) // remove
   return {
     token: token,

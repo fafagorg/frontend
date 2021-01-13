234fd7458c64dcab41112debeee8eab451186573
CesarGP <cgpcsjcmp@gmail.com>
2021-01-13 19:03:18 +0100

    feat: persisted redux state

--- src/index.js
+++ src/index.js
@@ -9,24 +9,33 @@ import "fontsource-roboto";
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from './reducers/reducer';
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
 // Router
 import { createBrowserHistory } from "history";
 import { HistoryContext } from './components/navigation/history'
 const customHistory = createBrowserHistory();
 
-
-const store = createStore(
-  reducer
-);
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+ 
+const persistedReducer = persistReducer(persistConfig, reducer);
+let store = createStore(persistedReducer);
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <HistoryContext.Provider value={customHistory}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </HistoryContext.Provider>
+    <PersistGate loading={null} persistor={persistor}>
+      <HistoryContext.Provider value={customHistory}>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </HistoryContext.Provider>
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );

16e6703676c5cff9553d01bfd995b35f3133cd99
Jesús Monda <jesusmonda9@gmail.com>
2021-01-24 18:20:23 +0100

    fix(): username chat

--- src/components/messenger/message/Message.js
+++ src/components/messenger/message/Message.js
@@ -8,7 +8,7 @@ class Message extends React.Component {
   render() {
     return (
       <>
-        <li class={this.props.dataa.userId === this.props.data.userId ? "sent" : "replies"}>
+        <li class={this.props.dataa.username === this.props.data.user.username ? "sent" : "replies"}>
           <p>{this.props.data.content}</p>
         </li>
       </>
@@ -19,7 +19,7 @@ class Message extends React.Component {
 function stateToProps(state) {
   return {
     token: state.userToken,
-    dataa: jwt.decode(state.userToken),
+    data: jwt.decode(state.userToken),
   }
 }
 export default connect(stateToProps)(withHistory(Message));

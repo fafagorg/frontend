a4851a19548ed32daeeec307575081727c118a49
Juan Miguel Blanco <juanmiblancof@gmail.com>
2021-02-11 18:29:05 +0100

    @juablafer - #12 - Tests fix

--- src/pages/messenger/Messenger.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Messenger from "./Messenger";
-
-test("renders learn react link", () => {
-  render(<Messenger />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});

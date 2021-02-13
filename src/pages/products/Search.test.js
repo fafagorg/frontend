25b8d4645635fc28c6af00b8edd39d6099ff87f2
alfonso bravo <alfobralla@hotmail.com>
2021-02-13 13:52:27 +0100

    products test improvement

--- src/pages/products/Search.test.js
+++ src/pages/products/Search.test.js
@@ -26,12 +26,25 @@ beforeEach(() => {
   container = document.createElement("div");
   document.body.appendChild(container);
 
-  nock('http://localhost:8080').defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-    .get('/api/v1/products/client/asd')
-    .reply(200, [{"name": "delete", "price":5, "category": "delete", "seller":"deleteTest"}]);
+  nock('http://localhost:8080').defaultReplyHeaders({ 'access-control-allow-origin': '*', 'access-control-allow-headers': 'Authorization' })
+    .post('/api/v1/products').times(100)
+    .reply(200, []);
+
+  nock('http://localhost:8080').defaultReplyHeaders({ 'access-control-allow-origin': '*', 'access-control-allow-headers': 'Authorization' })
+    .options('/api/v1/products').times(100)
+    .reply(200, {});
+
 
   nock('http://localhost:8080').defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-    .get('/api/v1/rates')
+    .get('/api/v1/products').times(100)
+    .reply(200, []);
+    
+  nock('http://localhost:8080').defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+  .get('/api/v1/products/client/RandomUser').times(100)
+  .reply(200, [{ "name": "This is a fantastic product for a test.", "price": 5, "category": "delete", "seller": "deleteTest" }]);
+
+  nock('http://localhost:8080').defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+    .get('/api/v1/rates').times(100)
     .reply(200, []);
 });
 
@@ -98,12 +111,13 @@ afterEach(() => {
 it("Should not allow a string in the price field", async () => {
   global.window = Object.create(window);
   const url = "https://frontend.fafago-dev.alexgd.es";
-  const username = 'asd';
+  const username = 'RandomUser'
   Object.defineProperty(window, 'location', {
     value: {
       location: url,
       pathname: '/product_client',
-      search: '?username=' + username
+      search: '?username=' + username,
+      reload: function () { }
     }
   });
 
@@ -113,11 +127,7 @@ it("Should not allow a string in the price field", async () => {
     </Provider>
   );
 
-  console.log('FIND PRODUCTS: ' + JSON.stringify(renderContent.find(Products).html()));
   renderContent.find(Products).setState(state);
-
-  console.log('RENDER CONTENT: ' + JSON.stringify(renderContent.html()));
-
 
   let titleInput = renderContent.find({ name: 'name' });
   let priceInput = renderContent.find({ name: 'price' });
@@ -134,15 +144,17 @@ it("Should not allow a string in the price field", async () => {
   expect(renderContent.find(Products).state().errorInfo).toBe('Price must be a number');
 });
 
+
 it("Should not allow a value lower than 1 in the price", async () => {
   global.window = Object.create(window);
   const url = "https://frontend.fafago-dev.alexgd.es";
-  const username = 'asd';
+  const username = 'RandomUser'
   Object.defineProperty(window, 'location', {
     value: {
       location: url,
       pathname: '/product_client',
-      search: '?username=' + username
+      search: '?username=' + username,
+      reload: function () { }
     }
   });
 
@@ -152,19 +164,12 @@ it("Should not allow a value lower than 1 in the price", async () => {
     </Provider>
   );
 
-  console.log('FIND PRODUCTS: ' + JSON.stringify(renderContent.find(Products).html()));
   renderContent.find(Products).setState(state);
-
-  console.log('RENDER CONTENT: ' + JSON.stringify(renderContent.html()));
-
-
   let titleInput = renderContent.find({ name: 'name' });
   let priceInput = renderContent.find({ name: 'price' });
   let button = renderContent.find({ 'data-testid': 'add' });
 
   priceInput.prop('onChange')({ target: { name: 'price', value: 0 } });
-  
-  
 
   button.simulate('click');
   renderContent.find(Products).render();
@@ -175,10 +180,11 @@ it("Should not allow a value lower than 1 in the price", async () => {
   expect(renderContent.find(Products).state().errorInfo).toBe('Price can not be lower than 1');
 });
 
-/*it("Should not allow a string in the price field", async () => {
+
+it("Should not allow to use a banned word in the name", async () => {
   global.window = Object.create(window);
-  const url = "https://frontend.fafago-dev.alexgd.es";
-  const username = 'asd';
+  const url = "https://frontend.fafago-dev.alexgd.es/";
+  const username = 'RandomUser'
   Object.defineProperty(window, 'location', {
     value: {
       location: url,
@@ -193,10 +199,201 @@ it("Should not allow a value lower than 1 in the price", async () => {
     </Provider>
   );
 
-  console.log('FIND PRODUCTS: ' + JSON.stringify(renderContent.find(Products).html()));
   renderContent.find(Products).setState(state);
 
-  console.log('RENDER CONTENT: ' + JSON.stringify(renderContent.html()));
+  let titleInput = renderContent.find({ name: 'name' });
+  let priceInput = renderContent.find({ name: 'price' });
+  let categoryInput = renderContent.find({ name: 'category' });
+  let button = renderContent.find({ 'data-testid': 'add' });
+
+  titleInput.prop('onChange')({ target: { name: 'name', value: "spam" } });
+  priceInput.prop('onChange')({ target: { name: 'price', value: 2 } });
+  categoryInput.prop('onChange')({ target: { name: 'category', value: 'CategoryTest' } });
+
+
+  button.simulate('click');
+  renderContent.find(Products).render();
+
+  //Wait for state change
+  await new Promise(resolve => setTimeout(() => resolve(), 500));
+
+  expect(renderContent.find(Products).state().errorInfo).toBe('Banned word used in name or category');
+});
+
+
+it("Category should not be empty", async () => {
+  global.window = Object.create(window);
+  const url = "https://frontend.fafago-dev.alexgd.es/";
+  const username = 'RandomUser'
+  Object.defineProperty(window, 'location', {
+    value: {
+      location: url,
+      pathname: '/product_client',
+      search: '?username=' + username
+    }
+  });
+
+  let renderContent = mount(
+    <Provider store={store}>
+      <Products />
+    </Provider>
+  );
+
+  renderContent.find(Products).setState(state);
+
+  let titleInput = renderContent.find({ name: 'name' });
+  let priceInput = renderContent.find({ name: 'price' });
+  let categoryInput = renderContent.find({ name: 'category' });
+  let button = renderContent.find({ 'data-testid': 'add' });
+
+  titleInput.prop('onChange')({ target: { name: 'name', value: "A nice product" } });
+  priceInput.prop('onChange')({ target: { name: 'price', value: 2 } });
+  categoryInput.prop('onChange')({ target: { name: 'category', value: '' } });
+
+
+  button.simulate('click');
+  renderContent.find(Products).render();
+
+  //Wait for state change
+  await new Promise(resolve => setTimeout(() => resolve(), 500));
+
+  expect(renderContent.find(Products).state().errorInfo).toBe('Category can not be empty');
+});
+
+
+it("Product creation should not return errors", async () => {
+  global.window = Object.create(window);
+  const url = "https://frontend.fafago-dev.alexgd.es/";
+  const username = 'RandomUser'
+  Object.defineProperty(window, 'location', {
+    value: {
+      location: url,
+      pathname: '/product_client',
+      search: '?username=' + username
+    }
+  });
+
+  let renderContent = mount(
+    <Provider store={store}>
+      <Products />
+    </Provider>
+  );
+
+  renderContent.find(Products).setState(state);
+
+  let titleInput = renderContent.find({ name: 'name' });
+  let priceInput = renderContent.find({ name: 'price' });
+  let categoryInput = renderContent.find({ name: 'category' });
+  let button = renderContent.find({ 'data-testid': 'add' });
+
+  titleInput.prop('onChange')({ target: { name: 'name', value: "A nice product" } });
+  priceInput.prop('onChange')({ target: { name: 'price', value: 2 } });
+  categoryInput.prop('onChange')({ target: { name: 'category', value: 'NiceCategory' } });
+
+
+  button.simulate('click');
+  renderContent.find(Products).render();
+
+  //Wait for state change
+  await new Promise(resolve => setTimeout(() => resolve(), 500));
+
+  expect(renderContent.find(Products).state().errorInfo).toBe(null);
+});
+
+
+
+it("Category should not have banned words", async () => {
+  global.window = Object.create(window);
+  const url = "https://frontend.fafago-dev.alexgd.es/";
+  const username = 'RandomUser'
+  Object.defineProperty(window, 'location', {
+    value: {
+      location: url,
+      pathname: '/product_client',
+      search: '?username=' + username
+    }
+  });
+
+  let renderContent = mount(
+    <Provider store={store}>
+      <Products />
+    </Provider>
+  );
+
+  renderContent.find(Products).setState(state);
+
+  let titleInput = renderContent.find({ name: 'name' });
+  let priceInput = renderContent.find({ name: 'price' });
+  let categoryInput = renderContent.find({ name: 'category' });
+  let button = renderContent.find({ 'data-testid': 'add' });
+
+  titleInput.prop('onChange')({ target: { name: 'name', value: "A nice product" } });
+  priceInput.prop('onChange')({ target: { name: 'price', value: 2 } });
+  categoryInput.prop('onChange')({ target: { name: 'category', value: 'spam' } });
+
+  button.simulate('click');
+  renderContent.find(Products).render();
+
+  //Wait for state change
+  await new Promise(resolve => setTimeout(() => resolve(), 500));
+
+  expect(renderContent.find(Products).state().errorInfo).toBe('Banned word used in name or category');
+});
+
+
+
+it("User RandomUser should have 1 product and should be listed", async () => {
+  global.window = Object.create(window);
+  const url = "https://frontend.fafago-dev.alexgd.es/";
+  const username = 'RandomUser';
+  Object.defineProperty(window, 'location', {
+    value: {
+      location: url,
+      pathname: '/product_client',
+      search: '?username=' + username
+    }
+  });
+
+  let renderContent = mount(
+    <Provider store={store}>
+      <Products />
+    </Provider>
+  );
+
+  //Wait for state change
+  await new Promise(resolve => setTimeout(() => resolve(), 500));
+
+  expect(renderContent.find(Products).state().errorInfo).toBe(null);
+  expect(renderContent.find(Products).html().includes("This is a fantastic product for a test.")).toBe(true);
+});
+
+
+
+
+
+
+/*it("Should not allow a string in the price field", async () => {
+  global.window = Object.create(window);
+  const url = "https://frontend.fafago-dev.alexgd.es";
+  const username = 'RandomUser'
+  Object.defineProperty(window, 'location', {
+    value: {
+      location: url,
+      pathname: '/product_client',
+      search: '?username=' + username
+    }
+  });
+
+  let renderContent = mount(
+    <Provider store={store}>
+      <Products />
+    </Provider>
+  );
+
+
+  renderContent.find(Products).setState(state);
+
+
 
 
   let titleInput = renderContent.find({ name: 'name' });
@@ -204,8 +401,8 @@ it("Should not allow a value lower than 1 in the price", async () => {
   let button = renderContent.find({ 'data-testid': 'add' });
 
   priceInput.prop('onChange')({ target: { name: 'price', value: 0 } });
-  
-  
+
+
 
   button.simulate('click');
   renderContent.find(Products).render();
@@ -234,10 +431,10 @@ it("Should not allow a string in the price field", async () => {
     </Provider>
   );
 
-  console.log('FIND PRODUCTS: ' + JSON.stringify(renderContent.find(Products).html()));
+
   renderContent.find(Products).setState(state);
 
-  console.log('RENDER CONTENT: ' + JSON.stringify(renderContent.html()));
+
 
 
   let titleInput = renderContent.find({ name: 'name' });
@@ -245,8 +442,8 @@ it("Should not allow a string in the price field", async () => {
   let button = renderContent.find({ 'data-testid': 'add' });
 
   priceInput.prop('onChange')({ target: { name: 'price', value: 0 } });
-  
-  
+
+
 
   button.simulate('click');
   renderContent.find(Products).render();
